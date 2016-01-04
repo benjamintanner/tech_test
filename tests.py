0c9f5b5f961dae05b658b9b566be20b5231ec10c
benjamintanner <bens.droid@gmail.com>
2016-01-04 13:16:45 -0500

    Update tests.py
    changes for main.py refactor

--- tests.py
+++ tests.py
@@ -29,25 +29,6 @@ class TestNumberOutput(unittest.TestCase):
         self.assertTrue(len(a_number.squares) == 9,msg="squares size = %d" % len(a_number.squares) )
         self.assertTrue(len(a_number.non_primes) == 65,msg="non primes size = %d" % len(a_number.non_primes) )
 
-    def test_weighted_array(self):
-        a_number= main.NUMBERS(0)
-        num = a_number.generate()
-        self.assertTrue(a_number.weighted_array.count('prime') == 3,msg="Weighted array prime count expected [3] got [%s]" %a_number.weighted_array.count('prime'))
-        self.assertTrue(a_number.weighted_array.count('non_prime') == 3, msg="Weighted array prime count expected [3] got [%d]" %a_number.weighted_array.count('non_prime'))
-        self.assertTrue(a_number.weighted_array.count('square') == 1, msg="Weighted array prime count expected [1] got [%d]" %a_number.weighted_array.count('squares'))
-
-        a_number= main.NUMBERS(2)
-        num = a_number.generate()
-        self.assertTrue(a_number.weighted_array.count('prime') == 6,msg="Weighted array prime count expected [6] got [%s]" %a_number.weighted_array.count('prime'))
-        self.assertTrue(a_number.weighted_array.count('non_prime') == 3, msg="Weighted array prime count expected [3] got [%d]" %a_number.weighted_array.count('non_prime'))
-        self.assertTrue(a_number.weighted_array.count('square') == 2, msg="Weighted array prime count expected [2] got [%d]" %a_number.weighted_array.count('squares'))
-
-        a_number= main.NUMBERS(10)
-        num = a_number.generate()
-        self.assertTrue(a_number.weighted_array.count('prime') == 30,msg="Weighted array prime count expected [3] got [%s]" %a_number.weighted_array.count('prime'))
-        self.assertTrue(a_number.weighted_array.count('non_prime') == 3, msg="Weighted array prime count expected [3] got [%d]" %a_number.weighted_array.count('non_prime'))
-        self.assertTrue(a_number.weighted_array.count('square') == 10, msg="Weighted array prime count expected [1] got [%d]" %a_number.weighted_array.count('squares'))
-
     def test_generate(self):
         #run this a million times and verify all output is valid
         for i in range(1,1000000):
@@ -61,21 +42,6 @@ class TestLetterOutput(unittest.TestCase):
         self.assertTrue(a_letter.letter_dictionary[2] =='b',msg="Position of 'b' is incorrect %s" %a_letter.letter_dictionary[2])
         self.assertTrue(a_letter.letter_dictionary[26] =='z',msg="Position of 'z' is incorrect %s" %a_letter.letter_dictionary[26])
         self.assertTrue(a_letter.vowel_dictionary[1] == 'a', msg="Position of 'a' is incorrect %s" %a_letter.vowel_dictionary[1])
-
-    def test_weighted_array(self):
-        test_value = 0
-        a_letter= main.LETTERS(test_value)
-        num = a_letter.generate()
-
-        self.assertTrue(a_letter.weighted_array.count('vowels') == a_letter.weighted_array.count('consonants'))
-        self.assertTrue(a_letter.weighted_array.count('Y') == a_letter.weighted_array.count('vowels') * 2 )
-
-        test_value= 10
-        a_letter= main.LETTERS(test_value)
-        num = a_letter.generate()
-
-        self.assertTrue(a_letter.weighted_array.count('vowels') == a_letter.weighted_array.count('consonants') * test_value )
-        self.assertTrue(a_letter.weighted_array.count('Y') == a_letter.weighted_array.count('vowels') *2 )
 
     def test_generate(self):
         #run this a million times and verify all output is valid
